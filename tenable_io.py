aef04ea0d89049fa5b156cc57916a52d48305518
root <root@masoolin-vm-u22.habana-labs.com>
2023-07-26 09:37:21 +0300

    new script update

--- tenable_io.py
+++ tenable_io.py
@@ -1,20 +1,26 @@
+import pprint
 from tenable.io import TenableIO
 
-scan_completed = True
-type(scan_completed)
-scan_in_progress = False
-type(scan_in_progress)
+scan_running = True
 
-
-
-scan_status=scan_in_progress
+#credentials = {"uuid":"null"}
 
 tio = TenableIO(
     access_key='cf81be6d9f5d5a01a197ccaec2010dca667e95f5ef2d4b14e0d2d18e4d4d36ab',
     secret_key='600a16e9302be0cc0f8ef3dd43f65190c968a9f81fbbbc5cf765803cc22e7c38'
 )
 
+for cred in tio.credentials.list():
+    print(cred)
+    if cred['name'] == 'Nessus SSH KEY':
+         uuid = cred["uuid"]
+#         id = cred["id"]
 
+
+print("uuid is", uuid)
+
+cred = tio.credentials.details(uuid)
+print(" cred is ", cred)
 
 #new_host_scan = tio.scans.create(
 #    name='ma_Habana_Example_scan',
@@ -24,11 +30,13 @@ tio = TenableIO(
 
 new_host_scan2 = tio.scans.create(
     name="ma_Habana_new_scan",
+    template='advanced',
+    credentials={'Host': {'SSH': [{'id': uuid}]}},
     scanner='nessus-tenable-srv',
-    Credentials = 'Nessus SSH KEY',
     targets=['10.111.59.72'],
 )
 
+print(new_host_scan2)
 scans = tio.scans.list()
 for scan in scans:
     print(f'Scan {scan["id"]} is named {scan["name"]}')
@@ -42,17 +50,17 @@ for scan in scans:
 
 print(new_scan_id)
 
-tio.scans.configure(new_scan_id, credentials='Nessus SSH KEY')
-tio.scans.launch(new_scan_id,targets=['10.111.59.72'])
+#tio.scans.configure(new_scan_id, credentials='Nessus SSH KEY')
+tio.scans.launch(new_scan_id)
 
 """
 
 tio.scans.launch(851,targets=['10.111.59.72'])
 """
-while scan_status == scan_in_progress:
+while scan_running == True:
         scan_status_reader = tio.scans.status(new_scan_id)
         if scan_status_reader == 'completed':
-            scan_status=scan_completed
+            scan_running=False
         else:
              print("scan in progress at status", scan_status_reader)
 
